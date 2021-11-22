96c44f704882a1a1ee48e22fbcfb845acc516c1d
IgorHamzi <igorwhamzi@gmail.com>
2021-11-22 18:18:32 -0300

    adiciona requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,42 @@
-// import React, { Component } from 'react';
-// import getUser from '../services/userAPI';
+import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Load from './Load';
 
-// export default class Header extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <header data-testid="header-component">
-//           <div data-testid="header-user-name">
-//             g
-//           </div>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: true,
+    };
+
+    this.user = this.user.bind(this);
+  }
+
+  componentDidMount() {
+    this.user();
+  }
+
+  async user() {
+    const { name } = await getUser();
+    this.setState({ name, loading: false });
+  }
+
+  render() {
+    const {
+      state: {
+        name,
+        loading,
+      },
+    } = this;
+    return (
+      <div>
+        <header data-testid="header-component">
+          {loading ? <Load /> : (
+            <div data-testid="header-user-name">{name}</div>
+          )}
+        </header>
+      </div>
+    );
+  }
+}
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Links from '../components/Links';
+import Header from '../components/Header';
 
 export default class Login extends Component {
   render() {
     return (
       <div data-testid="page-search">
+        <Header />
         <Links />
         <h1>opa</h1>
       </div>
