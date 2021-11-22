9decabfec84de8e7b5767d99bb2f76a8ce617cdb
IgorHamzi <igorwhamzi@gmail.com>
2021-11-22 18:30:00 -0300

    adiciona requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Load from './Load';
 
@@ -36,6 +37,9 @@ export default class Header extends Component {
             <div data-testid="header-user-name">{name}</div>
           )}
         </header>
+        <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </div>
     );
   }
