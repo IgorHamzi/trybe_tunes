fb257465e518735c0f4084904c78a54e40e2f7b4
IgorHamzi <igorwhamzi@gmail.com>
2021-11-22 21:16:28 -0300

    adiciona requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Load from '../components/Load';
 
 export default class Login extends Component {
   constructor() {
@@ -7,10 +10,22 @@ export default class Login extends Component {
     this.state = {
       bandName: '',
       buttonDisabled: true,
+      loading: false,
+      listBand: [],
+      renderList: false,
+      renderBandName: '',
     };
 
     this.enableDisableBtn = this.enableDisableBtn.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.onClickButton = this.onClickButton.bind(this);
+    this.searchBand = this.searchBand.bind(this);
+    this.listAlbum = this.listAlbum.bind(this);
+  }
+
+  onClickButton(event) {
+    event.preventDefault();
+    this.searchBand();
   }
 
   onInputChange({ target: { value, name } }) {
@@ -29,38 +44,87 @@ export default class Login extends Component {
     }
   }
 
+  async searchBand() {
+    const { bandName } = this.state;
+    this.setState({ loading: true });
+    const list = await searchAlbumsAPI(bandName);
+    this.setState({
+      loading: false,
+      renderBandName: bandName,
+      bandName: '',
+      listBand: list,
+      renderList: true,
+    });
+  }
+
+  listAlbum() {
+    const { listBand, renderBandName } = this.state;
+    if (listBand.length === 0) {
+      return (<p>Nenhum álbum foi encontrado</p>);
+    } return (
+      <div>
+        <h1>{`Resultado de álbuns de: ${renderBandName}`}</h1>
+        {listBand.map(({
+          artistId,
+          artworkUrl100,
+          artistName,
+          collectionName,
+          collectionId,
+        }) => (
+          <div key={ artistId }>
+            <Link
+              to={ `/album/${collectionId}` }
+              data-testid={ `link-to-album-${collectionId}` }
+            >
+              <img src={ artworkUrl100 } alt={ collectionName } />
+              <p>{artistName}</p>
+              <p>{collectionName}</p>
+              Album
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   render() {
     const {
       state: {
         bandName,
         buttonDisabled,
+        loading,
+        renderList,
       },
       onInputChange,
-      // onClickButton,
+      onClickButton,
+      listAlbum,
     } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="bandName">
-            nome da banda:
-            <input
-              data-testid="search-artist-input"
-              name="bandName"
-              type="text"
-              value={ bandName }
-              onChange={ onInputChange }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ buttonDisabled }
-            // onClick={ onClickButton }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading ? <Load /> : (
+          <form>
+            <label htmlFor="bandName">
+              nome da banda:
+              <input
+                data-testid="search-artist-input"
+                name="bandName"
+                type="text"
+                value={ bandName }
+                onChange={ onInputChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ buttonDisabled }
+              onClick={ onClickButton }
+            >
+              Pesquisar
+            </button>
+          </form>
+        )}
+        {renderList && listAlbum()}
       </div>
     );
   }
